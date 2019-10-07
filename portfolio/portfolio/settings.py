eccba26a5cb0144751d4a3c8f470ca271c9e2157
Denis John <denis.john.ndaraek@gmail.com>
2019-10-07 02:58:13 +0100

    Created jobs model and registered it to our admin

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
