d9e09860b125b9893dbf354ab49dcab9f6732573
Denis John <denis.john.ndaraek@gmail.com>
2019-10-10 12:25:42 +0100

    Completed our portfolio site

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def allblogs(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs' : blogs
+    }
+    return render(request, 'blog/allblogs.html', context)
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    context = {
+        'blog': detailblog
+    }
+    return render(request, 'blog/detail.html', context)
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +9,5 @@ from jobs.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
