b6caf65a4e39c4d656e6c71a74f192737fd221d8
Denis John <denis.john.ndaraek@gmail.com>
2019-10-16 19:21:45 +0100

    Completed postgres DB setup

--- producthunt/producthunt/settings.py
+++ producthunt/producthunt/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthuntdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
