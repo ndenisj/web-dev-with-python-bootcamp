5142d3ee738f4ecc44cd500d45123771809f248c
Denis John <denis.john.ndaraek@gmail.com>
2019-10-23 22:50:13 +0100

    Completed create new product

--- producthunt/product/views.py
+++ producthunt/product/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Product
 
 def home(request):
     context = {
         'page_title': 'Welcome to product hunt',
     }
     return render(request, 'product/home.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        product = Product()
+        product.title = request.POST['title']
+        product.body = request.POST['body']
+        product.icon = request.FILES['icon']
+        product.image = request.FILES['image']
+        if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+            product.url = request.POST['url']
+        else:
+            product.url = 'htt://' + request.POST['url']
+        product.hunter = request.user
+
+        ################
+        #product.pub_date = timezone.datetime.now()
+        #product.votes_total = 1
+        product.save()
+        #redirect to details
+        return redirect('home')
+    else:
+        context = {
+            'page_title': 'Create new product',
+        }
+        return render(request, 'product/create.html', context)
--- producthunt/producthunt/urls.py
+++ producthunt/producthunt/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('account/', include('account.urls')),
+    path('product/', include('product.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
